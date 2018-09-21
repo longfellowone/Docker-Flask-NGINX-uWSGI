0d22b6de44133a4e1b3356b44638b2fd03c30c8b
Matt <mattwright3195@gmail.com>
2018-09-20 22:05:36 -0700

    still working

--- app/app.py
+++ app/app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!!!!!!!!'
+    return 'Hello, World!!'
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80)
+
+#docker-compose up --force-recreate
